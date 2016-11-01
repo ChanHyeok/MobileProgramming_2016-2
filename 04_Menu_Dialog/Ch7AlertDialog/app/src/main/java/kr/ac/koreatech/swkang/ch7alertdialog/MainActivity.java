ccf54d82c7234c7994682ba57306b674c5b7ef5a
skang-koreatech <swkang@koreatech.ac.kr>
2016-11-01 14:01:05 +0900

    no message

--- 04_Menu_Dialog/Ch7AlertDialog/app/src/main/java/kr/ac/koreatech/swkang/ch7alertdialog/MainActivity.java
+++ 04_Menu_Dialog/Ch7AlertDialog/app/src/main/java/kr/ac/koreatech/swkang/ch7alertdialog/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -34,6 +35,36 @@ public class MainActivity extends AppCompatActivity {
                 showDialog(DIALOG_COLOR_MESSAGE);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MobileProgramming", "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MobileProgramming", "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MobileProgramming", "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MobileProgramming", "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MobileProgramming", "onDestory()");
     }
 
     @Override
